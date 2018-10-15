c1e6b42a324c07e214cd9a4b843d907c7f5bb064
nursh <nsheikhmohammed@yahoo.com>
2018-10-15 11:10:49 -0400

    Initial commit

---
+++ worker/index.js
@@ -0,0 +1,20 @@
+const redis = require('redis');
+
+const redisClient = redis.createClient({
+  host: process.env.redisHost,
+  port: process.env.redisPort,
+  retry_strategy: () => 1000,
+});
+
+const sub = redisClient.duplicate();
+
+function fib(index) {
+  if (index < 2) return 1;
+  return fib(index - 1) + fib(index - 2);
+}
+
+sub.on('message', (channel, message) => {
+  redisClient.hset('values', message, fib(parseInt(message)));
+})
+
+sub.subscribe('insert');
