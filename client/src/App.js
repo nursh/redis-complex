a8e96dede32fbe2d8f2e9cbb84849d607e704c25
nursh <nsheikhmohammed@yahoo.com>
2018-10-20 16:02:42 -0400

    Added dockerfiles to client, server and worker folders

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,28 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import OtherPage from './OtherPage';
-import Fib from './Fib';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import OtherPage from "./OtherPage";
+import Fib from "./Fib";
 
+const Header = () => (
+  <header>
+    <Link to="/">Home</Link>
+    <Link to="/otherpage">Other Page</Link>
+  </header>
+);
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Router>
-          <header className="App-header">
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
           <div>
+            <Header />
             <Route exact to="/" component={Fib} />
             <Route to="/otherpage" component={OtherPage} />
           </div>
         </Router>
-
       </div>
     );
   }
