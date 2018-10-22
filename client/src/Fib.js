a161e149a92d6f00d0de1da938be3ac7ac590225
nursh <nsheikhmohammed@yahoo.com>
2018-10-22 19:13:38 -0400

    Added docker services for both the worker and client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
 
 class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: ''
-  }
+    index: ""
+  };
 
   componentDidMount() {
     this.fetchValues();
@@ -14,25 +14,25 @@ class Fib extends Component {
   }
 
   async fetchValues() {
-    const values = await axios.get('http://localhost:5000/values/current');
+    const values = await axios.get("/api/values/current");
     this.setState({ values: values.data });
   }
 
   async fetchIndexes() {
-    const seenIndexes = await axios.get('http://localhost:5000/values/all');
+    const seenIndexes = await axios.get("/api/values/all");
     this.setState({ seenIndexes: seenIndexes.data });
   }
 
-  handleSubmit = async (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
-    await axios.post('/values', {
-      index: this.state.index,
+    await axios.post("/api//values", {
+      index: this.state.index
     });
-    this.setState({ index: ''});
-  }
+    this.setState({ index: "" });
+  };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({number}) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => number).join(", ");
   }
 
   renderValues() {
@@ -43,7 +43,7 @@ class Fib extends Component {
         <div key={key}>
           For index {key}, I calculated {this.state.values[key]}
         </div>
-      )
+      );
     }
     return entries;
   }
